964135d3f137575a14b34877ed03cb7925278610
juniomenchik <juniomenchik@live.com>
2026-02-11 18:01:06 -0300

    [master]: Railway release configuration 0.0.1

--- src/main/java/com/aincrad/know_recipes_be/KnowRecipesBeApplication.java
+++ src/main/java/com/aincrad/know_recipes_be/KnowRecipesBeApplication.java
@@ -1,7 +1,5 @@
 package com.aincrad.know_recipes_be;
 
-import com.aincrad.know_recipes_be.service.RecipeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KnowRecipesBeApplication implements CommandLineRunner {
 
-	@Autowired
-	private RecipeService recipeService;
 
 	public static void main(String[] args) {
 
@@ -21,7 +17,7 @@ public class KnowRecipesBeApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-//		recipeService.generateTenRecipesForUser(1L);
+		System.out.println(System.getenv("SPRING_DATASOURCE_URL"));
 	}
 
 }
